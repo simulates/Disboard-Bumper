083ef6d812336d1193ae5887c470a84a630c924d
e2 <52636202+simulates@users.noreply.github.com>
2021-07-07 20:41:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-import asyncio, discord, random, time, json
+import asyncio
+import discord
+import random
+import time
+
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix=".", self_bot=True)
+
+token = "token :D"
+num = random.randint(7263, 7500)
 
 @bot.command(pass_context=True)
 async def bump(ctx):
     await ctx.message.delete()
     while True:
         await ctx.send('!d bump')
-        time.sleep(random.randint(7263, 7500))
+        time.sleep(num)
 
 @bot.command(pass_context=True)
 async def ping(ctx):
     await ctx.send(f"pong! {round(bot.latency * 1000)}ms")
-
-
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Streaming(name=f"kisses", url="https://www.youtube.com/watch?v=DLzxrzFCyOs"))
@@ -22,4 +27,4 @@ async def on_ready():
     print(bot.user.id)
 
 
-bot.run(config['token'], bot=False)
+bot.run(token, bot=False)
